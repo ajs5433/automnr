0e2aa297e3d2763265dbe5ad1af57339fc886ffe
BertoTech <ajs5433@rit.edu>
2018-07-04 14:31:24 -0400

    added the parser and password verification

--- main.py
+++ main.py
@@ -1,33 +1,90 @@
 #!/usr/bin/python
-from FileHandler import FileHandler
 
 import os
+import optparse
+import requests
 
-#-------------------------
-#           DATA
-#-------------------------
+from FileHandler import FileHandler
+
+
+#--------------------------------------------------------------------------------
+#                                   DATA                                         
+#--------------------------------------------------------------------------------
 
 # file data
-location    = ""
-filename    = "nrr_data"
+LOCATION    = ""
+FILENAME    = "nrr_data"
 
-#encryption data
-key         = "newrelicexec"
-blocksize   = 256
+# encryption data
+KEY         = "newrelicexec"
+BLOCK_SIZE  = 256
 
+# website data
+URL     =  'https://login.newrelic.com/login'
 
-if __name__=='__main__':
-  
-  file = location+filename
-  
-  
+#--------------------------------------------------------------------------------
+
+def enter_credentials():
   while(True):
-    try:
-      if(os):
+    username  = raw_input("Please enter your newrelic username: ")
+    username_ = raw_input("To verify, please re-enter username: ")
+    passwd    = getpass.getpass("Enter your password: ")
+    passwd_   = getpass.getpass("Re-enter password: ")
     
-    except Exception as e:
-      print(e)
-      print("Try again...")
+    if not username == username_:
+      print("Usernames did not match, please try again")
       continue
+    
+    if not passwd == passwd_ :
+      print("Passwords did not match, please try again")
+      continue
+    break
+  return username, password
+  
+def check_credentials(username, password):
+  payload = {
+  'login[email]'      : username,
+  'login[password]'   : password,
+  'login[remember_me]': '0',
+  'commit' : 'Sign in'
+  }
+  with requests.session() as s:
+    post = s.post(URL, data=payload)
+  return not "Unable to log in" in post.text
+
+def create_new_password(message):
+  print(message)
+  error_count = 0
+  while(True):
+    username, password  = enter_credentials()
+    login_successfull   = check_credentials(username, password)
+    
+    if not login_successfull and error_count == 3:
+      print("You attempted the username and passwords too many times!. Exiting Program... ")
+      exit(1)
+    elif not login_successfull:
+      print("Unable to log in. Please ensure that your email and password are correct.\n")
+      continue
+    break
+  
+if __name__=='__main__':
+
+  parser = optparse.OptionParser(usage=" ")
+  parser.add_option("-p", "--passwd", dest="changepasswd", type='bool', 
+                    help=("This option is used to change the password [default: %default]"))
+                    
+  parser.set_defaults(changepasswd=False)
+  opts, args = parser.parse_args()
+  
+  if os.name != 'nt':
+    print("We apologize, this program was designed to run only on Windows machines. Exiting ...")
+    exit(1)
+  
+  file = LOCATION + FILENAME
+  if not os.path.isfile(file): 
+    create_new_password("It looks like you have never set up your account\n")
+  elif (opts.changepasswd==True):
+    create_new_password("You chose the create_new_password option!\n")
+      
   
   
